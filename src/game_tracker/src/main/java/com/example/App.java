0578aebf863a5b3e9b42f6287fd4879ae28ab0f3
Jessee Horton <61556428+Jessku@users.noreply.github.com>
2025-06-27 14:18:27 -0400

    Tested Create method for UserDAO successfully

--- src/game_tracker/src/main/java/com/example/App.java
+++ src/game_tracker/src/main/java/com/example/App.java
@@ -19,12 +19,16 @@ public class App
         DAOInterface userDAO = new UserDataDAO(); //DAO for UserData
         List<UserData> userList = userDAO.getAll(); //Initial user list
         UserData newUser1 = new UserData("Hatsune Miku", "W0rld15M1n3"); //New user to be added
-         UserData newUser2 = new UserData("Axl Low", "OutOfTheBox"); //New user to be added
+        UserData newUser2 = new UserData("Axl Low", "OutOfTheBox"); //New user to be added
 
 
         System.out.println( "Hello World!" );
+
+        //UserDataDAO.getAll()
         userList.forEach(e-> {System.out.println(e.toString());});
         System.out.println();
+
+        //UserDataDAO.create()
         try {
             if(userDAO.create(newUser1) && userDAO.create(newUser2)) {
                 userList = userDAO.getAll(); //Refresh the user list after adding a new user
@@ -35,6 +39,8 @@ public class App
         } catch (SQLException e1) {
             e1.printStackTrace();
         }
+
+        //UserDataDAO.getById()
 
          
 
