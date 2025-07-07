ea7309e74035a52e7df8ccc564332e230fe67c64
Jessee Horton <61556428+Jessku@users.noreply.github.com>
2025-07-06 23:39:05 -0400

    Finished initial games in GameData database

--- GameData.sql
+++ GameData.sql
@@ -1,27 +1,149 @@
 --Input data
 
---PS5/4 games
+--PS5 games
 INSERT INTO GameData (game_name, game_platform) VALUES ('Persona 3: Reload', 'PS5');
 INSERT INTO GameData (game_name, game_platform) VALUES ('Elden Ring', 'PS5');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Returnal', 'PS5');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Frontiers', 'PS5');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Scarlet Nexus', 'PS5');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Tales of Arise', 'PS5');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Assassin''s Creed Valhalla', 'PS5');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Monster Hunter Wilds', 'PS5');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Spider-Man 2', 'PS5');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Final Fantasy XVI', 'PS5');
+
+--PS4 games
 INSERT INTO GameData (game_name, game_platform) VALUES ('Gravity Rush', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Persona 5 Strikers', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Gravity Rush', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Spider-Man', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('inFamous SecondSon', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('NieR: Automata', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('NieR Replicant ver.1.22474487139...', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Hatsune Miku: Project Diva X', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Batman Arkham Knight', 'PS4');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Colors: Ultimate', 'PS4');
+
 
 --Switch games
 INSERT INTO GameData (game_name, game_platform) VALUES ('Triangle Strategy', 'Switch');
 INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Scarlet', 'Switch');
 INSERT INTO GameData (game_name, game_platform) VALUES ('Octopath Traveler', 'Switch');
 INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Legends: Arceus', 'Switch');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Persona 5 Royal', 'Switch');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Super Smash Bros. Ultimate', 'Switch');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Octopath Traveler II', 'Switch');
+INSERT INTO GameData (game_name, game_platform) VALUES ('The Legend of Zelda: Breath of the Wild', 'Switch');
+INSERT INTO GameData (game_name, game_platform) VALUES ('The Legend of Zelda: Tears of a Kingdom', 'Switch');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Phoenotopia: Awakening', 'Switch');
 
 --PC games
 INSERT INTO GameData (game_name, game_platform) VALUES ('Date Everything', 'Steam');
 INSERT INTO GameData (game_name, game_platform) VALUES ('Kingdom Hearts 1.5 + 2.5', 'Epic Games');
 INSERT INTO GameData (game_name, game_platform) VALUES ('Titantfall 2', 'EA Launcher');
+INSERT INTO GameData (game_name, game_platform) VALUES ('MiSide', 'Steam');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Cyberpunk 2077', 'Steam');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Doki Doki Literature Club', 'Steam');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Doki Doki Literature Club Plus!', 'Steam');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Helltaker', 'Steam');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Resonance of Fate End of Eternity 4KHD Edition', 'Steam');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Palworld', 'Steam');
 
 --GameCube games
 INSERT INTO GameData (game_name, game_platform) VALUES ('Shadow The Hedgehog', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Super Smash Bros. Melee', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Tales of Symphonia', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Adventure 2: Battle', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Adventure DX: Director''s Cut', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Power Rangers: Dino Thunder', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fire Emblem: Path of Radiance', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Teen Titans', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Heroes', 'GameCube');
+INSERT INTO GameData (game_name, game_platform) VALUES ('One Piece: Grand Battle', 'GameCube');
 
 --PS3 games
+INSERT INTO GameData (game_name, game_platform) VALUES ('Hatsune Miku: Project Diva F 2nd', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic The Hedgehog', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Infamous', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('PlayStation All-Stars Battle Royale', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Final Fantasy XIII', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Final Fantasy XIII-2', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Lightning Returns: Final Fantasy XIII', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Infamous 2', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Uncharted: Drake''s Fortune', 'PS3');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Mortal Kombat', 'PS3');
 
 --Wii U games
+INSERT INTO GameData (game_name, game_platform) VALUES ('Super Smash Bros. for Wii U', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Tokyo Mirage Sessions #FE', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('The Wonderful 101', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Splatoon', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Nintendo Land', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Metroid Prime Trilogy', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Legend of Zelda: The Wind Waker HD', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Batman Arkham Origins', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Mario Kart 8', 'Wii U');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Lost World', 'Wii U');
+
+--Nintendo DS games
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Diamond Version', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Pearl Version', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Platinum Version', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Black Version', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon White Version', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Black 2 Version', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon White 2 Version', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Chronicles: The Dark Brotherhood', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Kingdom Hearts 358/2 Days', 'NDS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fossil Fighters', 'NDS');
+
+--Nintendo 3DS games
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon X Version', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Y Version', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Kingdom Hearts 3D: Dream Drop Distance', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Sun Version', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Moon Version', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fire Emblem: Awakening', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fire Emblem: Birthright', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fire Emblem: Conquest', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fire Emblem: Revelations', '3DS');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Mario & Luigi: Bowser''s Inside Story + Bowser Jr''s Journey', '3DS');
+
+--Gameboy Advance games
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fire Emblem', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fire Emblem: The Sacred Stones', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon LeafGreen Version', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon FireRed Version', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Ruby Version', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Sapphire Version', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Emerald Version', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Advance 3', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Final Fantasy I & II: Dawn of Souls', 'GBA');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Battle', 'GBA');
+
+--Wii games
+INSERT INTO GameData (game_name, game_platform) VALUES ('Fire Emblem: Radiant Dawn', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Super Smash Bros. Brawl', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Trauma Team', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('One Piece: Unlimited Adventure', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sakura Wars: So Long, My Love', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Final Fantasy Crystal Chronicles: My Life as a Darklord', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Xenoblade Chronicles', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Epic Mickey', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Pokemon Rumble', 'Wii');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Guilty Gear XX Λ Core', 'Wii');
+
+--PS2 games
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sonic Heroes', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sly Cooper and the Thievius Raccoonus', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sly 2: Band of Thieves', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Sly 3: Honor Among Thieves', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Kingdom Hearts', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Kingdom Hearts II', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Robot Alchemic Drive', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('One Piece: Grand Adventure', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Final Fantasy X', 'PS2');
+INSERT INTO GameData (game_name, game_platform) VALUES ('Final Fantasy X-2', 'PS2');
 
 --ItemLists
 INSERT INTO ListItems (list_id, game_id, item_status) VALUES (1, 2, 'not_started');
